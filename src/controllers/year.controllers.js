4133f74c2832ee4f20e3506549e5bda8cf351a34
MauricioPerezIsa <mauricioexequielperezisa@gmail.com>
2023-07-29 20:46:19 -0300

    Crear funcion de showYear alternar el estado de las cuotas

--- src/controllers/year.controllers.js
+++ src/controllers/year.controllers.js
@@ -1,38 +1,8 @@
 const Year = require("../models/year.model")
 
-// const showYear = async (req, res) => {
-//     try {
-//         const year = await Year.findOne({idAlumno, anio})
-//         if (year === null) {
-//             res.status(404)
-//             return res.json({ message: "Id no encontrado" })
-//         }
-//         const anioRegex= new RegExp(req.query.anio)
-//         const filters={
-//             anio:{$regex:anioRegex}
-//         }
-//         const cuota = await Year.find(filters)
-//     }
-//     catch(error){ console.log(error)
-//         res.status(500).json({message:"error al mostrar la cuota"})
-//     }
-
-// //recibir param idAlumno
-// //anio cursando
-
-// //con esto hacer filtrado y mostrar
-
-// }
-const updateYear = async(req,res)=>{
+const showYear = async (req, res) => {
     const idYear = req.params.id; // Obtener el id del documento Year desde los parámetros de la solicitud
-    const cuotaName = req.params.cuota; // Obtener el nombre de la cuota desde los parámetros de la solicitud
-  
-    // Verificar si el nombre de la cuota recibida es válido
-    const validCuotas = ["cuotaUno", "cuotaDos", "cuotaTres", "cuotaCuatro", "cuotaCinco", "cuotaSeis"];
-    if (!validCuotas.includes(cuotaName)) {
-      return res.status(400).json({ message: "Nombre de cuota no válido" });
-    }
-  
+
     try {
       // Buscar el documento Year por su id
       const year = await Year.findById(idYear);
@@ -41,15 +11,54 @@ const updateYear = async(req,res)=>{
         return res.status(404).json({ message: "Year no encontrado" });
       }
   
-      // Alternar el estado de la cuota específica
-      year[cuotaName] = !year[cuotaName];
+      // Obtener todas las cuotas y sus estados
+      const cuotas = {
+        cuotaUno: year.cuotaUno,
+        cuotaDos: year.cuotaDos,
+        cuotaTres: year.cuotaTres,
+        cuotaCuatro: year.cuotaCuatro,
+        cuotaCinco: year.cuotaCinco,
+        cuotaSeis: year.cuotaSeis,
+        cuotaSiete: year.cuotaSiete,
+        cuotaOcho: year.cuotaOcho,
+        cuotaNueve: year.cuotaNueve,
+        cuotaDiez: year.cuotaDiez
+      };
   
-      // Guardar los cambios en la base de datos
-      await year.save();
-  
-      res.json({ message: 'Estado de la cuota'+cuotaName+'alternado correctamente', year });
+      res.json(cuotas);
     } catch (error) {
-      res.status(500).json({ message: "Error al alternar el estado de la cuota", error: error.message });
+      res.status(500).json({ message: "Error al obtener las cuotas", error: error.message });
     }
 }
-module.exports = {updateYear}
+
+
+const updateYear = async (req, res) => {
+    const idYear = req.params.id; // Obtener el id del documento Year desde los parámetros de la solicitud
+    const cuotaName = req.params.cuota; // Obtener el nombre de la cuota desde los parámetros de la solicitud
+
+    // Verificar si el nombre de la cuota recibida es válido
+    const validCuotas = ["cuotaUno", "cuotaDos", "cuotaTres", "cuotaCuatro", "cuotaCinco", "cuotaSeis","cuotaSiete","cuotaOcho","cuotaNueve","cuotaDiez"];
+    if (!validCuotas.includes(cuotaName)) {
+        return res.status(400).json({ message: "Nombre de cuota no válido" });
+    }
+
+    try {
+        // Buscar el documento Year por su id
+        const year = await Year.findById(idYear);
+
+        if (!year) {
+            return res.status(404).json({ message: "Year no encontrado" });
+        }
+
+        // Alternar el estado de la cuota específica
+        year[cuotaName] = !year[cuotaName];
+
+        // Guardar los cambios en la base de datos
+        await year.save();
+
+        res.json({ message: 'Estado de la cuota' + cuotaName + 'alternado correctamente', year });
+    } catch (error) {
+        res.status(500).json({ message: "Error al alternar el estado de la cuota", error: error.message });
+    }
+}
+module.exports = { updateYear, showYear }
--- src/routes/year.route.js
+++ src/routes/year.route.js
@@ -1,19 +1,17 @@
 const { Router } = require("express")
 const { check, param, body } = require("express-validator")
 const { expressValidations } = require("../middleware/common.validation")
-const { updateYear } = require("../controllers/year.controllers")
+const { updateYear, showYear } = require("../controllers/year.controllers")
 
 const yearRouter = Router()
 
-// http://localhost:8000/year/show
-// yearRouter.get('/show/:id', [
-//     param('idAlumno', 'El Id del alumno debe ser valido').isMongoId(),
-//     body("anio", "Debe ingresar un año").notEmpty(),
-//     body("anio", "El año debe Ser 1,2,3,4").isNumeric()
-// ], expressValidations,
-//     showYear)
+http://localhost:8000/year/show
+yearRouter.get('/show/:id', [
+    check("id").isMongoId().withMessage("El id del año debe ser válido")],
+    expressValidations,
+    showYear)
 
 yearRouter.put("/update-year/:id/:cuota", [check("id").isMongoId().withMessage("El id del año debe ser válido"),
-check("cuota").matches(/^(cuotaUno|cuotaDos|cuotaTres|cuotaCuatro|cuotaCinco|cuotaSeis)$/).withMessage("Nombre de cuota no válido")],
+check("cuota").matches(/^(cuotaUno|cuotaDos|cuotaTres|cuotaCuatro|cuotaCinco|cuotaSeis|cuotaSiete|cuotaOcho|cuotaNueve|cuotaDiez)$/).withMessage("Nombre de cuota no válido")],
 expressValidations, updateYear)
 module.exports = yearRouter
