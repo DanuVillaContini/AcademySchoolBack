ae1ec8844925227ebf9230ffe0b8a649b29443bd
MauricioPerezIsa <mauricioexequielperezisa@gmail.com>
2023-08-13 16:56:43 -0300

    Eliminadas cosas sin uso de main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const express = require('express')
 const mongoose = require("mongoose");
 const alumnoRouter = require('./src/routes/alumnos.route');
 const personalRouter = require('./src/routes/personal.route');
-const notasRouter = require('./src/routes/notas.route');
 const materiaRouter = require('./src/routes/materia.route');
 const { iniciarSuperUsuarioDB, iniciarInstitutoDB } = require("./src/utils/inicio.utils");
 const yearRouter = require('./src/routes/year.route');
@@ -22,7 +21,6 @@ app.use(express.json({ limit: "50mb" }))
 
 app.use("/alumno", alumnoRouter)
 app.use("/personal", personalRouter)
-app.use("/notas", notasRouter) //aliminar
 app.use("/materias", materiaRouter)
 app.use("/year",yearRouter)
 app.use("/auth", authRouter)
